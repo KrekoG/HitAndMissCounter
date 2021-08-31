b113f6a08654c4a630f9bee6de3394d2817282d0
krekog <kreko.gergely@gmail.com>
2021-08-31 22:09:46 +0100

    first up

---
+++ HAM.lua
@@ -0,0 +1,62 @@
+function HAM_init()
+  if not(HAM) then
+    HAM_reset()
+  end
+  HAM["record"] = false
+
+  SLASH_HAM1 = "/ham"
+
+  function SlashCmdList.HAM(msg)
+    if msg =="dump" then
+      DEFAULT_CHAT_FRAME:AddMessage("Dumping HAM data:")
+      for key, value in pairs(HAM["data"]) do
+        DEFAULT_CHAT_FRAME:AddMessage(key .. " - " .. value)
+      end
+    elseif msg =="sum" then
+      DEFAULT_CHAT_FRAME:AddMessage("Total: " .. HAM["hit"] + HAM["miss"])
+      DEFAULT_CHAT_FRAME:AddMessage("Hits: " .. HAM["hit"])
+      DEFAULT_CHAT_FRAME:AddMessage("Misses: " .. HAM["miss"])
+    elseif msg =="reset" then
+      HAM_reset()
+      DEFAULT_CHAT_FRAME:AddMessage("HAM is reset and is no longer recording")
+    elseif msg =="record" then
+      if HAM["record"] then
+        HAM["record"] = false
+        DEFAULT_CHAT_FRAME:AddMessage("HAM is no longer recording")
+      else
+        HAM["record"] = true
+        table.insert(HAM["data"], "startup")
+        DEFAULT_CHAT_FRAME:AddMessage("HAM is now recording")
+      end
+    else
+      if HAM["record"] then
+        DEFAULT_CHAT_FRAME:AddMessage("HAM is currently recording")
+      else
+        DEFAULT_CHAT_FRAME:AddMessage("HAM is currently not recording")
+      end
+      DEFAULT_CHAT_FRAME:AddMessage("/ham sum - for summary")
+      DEFAULT_CHAT_FRAME:AddMessage("/ham dump - for data dump")
+      DEFAULT_CHAT_FRAME:AddMessage("/ham reset - to reset the addon")
+      DEFAULT_CHAT_FRAME:AddMessage("/ham record - to toggle recording")
+    end
+  end
+end
+
+function HAM_reset()
+  HAM = {}
+  HAM["record"] = false
+  HAM["data"] = {}
+  HAM["hit"] = 0
+  HAM["miss"] = 0
+end
+
+function HAM_log(event)
+  if not(HAM["record"]) then return end
+  if event == "CHAT_MSG_COMBAT_SELF_HITS" then
+    table.insert(HAM["data"], "hit")
+    HAM["hit"] = HAM["hit"] + 1
+  elseif event == "CHAT_MSG_COMBAT_SELF_MISSES" then
+    table.insert(HAM["data"], "miss")
+    HAM["miss"] = HAM["miss"] + 1
+  end
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+
+# Hit and miss counter
+A WoW 1.12 addon that allows you to record your hits and misses
+
+Use the command: `/ham`
+
+## Installation instructions
+1. Press the green "code" button -> Download ZIP
+2. Extract the `HitAndMissCounter-main.zip` file and rename the extracted folder to `HitAndMissCounter`, then move it to your client's `\Interface\AddOns\` folder.
+3. Start/Restart your game
